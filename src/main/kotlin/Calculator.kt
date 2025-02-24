74b1bf452046b11d634a7c33839a9e9d2d1eabf3
O Csm de JP <yp.devkl@gmail.com>
2025-02-23 22:24:07 -0600

    Modified Calculator's methods exceptions, making the code more dry and more functional

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,23 +1,32 @@
 package org.example
 
 class Calculator{
+    private fun edgeRestriction(result : Double) : Boolean{
+        return result >= -Double.MAX_VALUE && result <= Double.MAX_VALUE
+    }
+
+    private fun divideByZeroRestriction(divisor : Double) : Boolean{
+        return divisor != 0.0
+    }
+
     fun add(a : Double, b : Double) : Double{
-        require(a + b <= Double.POSITIVE_INFINITY){"The sum of arguments is bigger than double max value"}
-        return a + b;
+        require(edgeRestriction(a + b)){"The sum of arguments over edges the limits of Double numbers"}
+        return a + b
     }
 
     fun subtract(a : Double, b : Double) : Double{
-        require(a - b >= Double.NEGATIVE_INFINITY){"The subtract of arguments is smaller than double min value"}
+        require(edgeRestriction(a - b)){"The subtract of arguments over edges the limits of Double numbers"}
         return a - b;
     }
 
     fun multiply(a : Double, b : Double) : Double{
-        require(a * b <= Double.POSITIVE_INFINITY){"The product of arguments is bigger than double max value"}
+        require(edgeRestriction(a * b)){"The product of arguments over edges the limits of Double numbers"}
         return a * b;
     }
 
     fun divide(a : Double, b : Double) : Double{
-        require(b != 0.0) {"Cannot divide by zero"}
-        return a/b
+        require(divideByZeroRestriction(b)) {"Cannot divide by zero"}
+        require(edgeRestriction(a / b)){"The quotie of arguments over edges the limits of Double numbers"}
+        return a / b
     }
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,6 +1,9 @@
 import org.example.Calculator
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class CalculatorTest {
     private lateinit var calculator: Calculator
@@ -12,46 +15,57 @@ class CalculatorTest {
 
     @Test
     fun testPositiveAddition(){
-
+        val res = calculator.add(10.0, 2.0);
+        assertEquals<Double>(res, 12.0)
     }
 
     @Test
     fun testNegativeAddition(){
-
+        val res = calculator.add(-10.0, -2.0);
+        assertEquals<Double>(res, -12.0)
     }
 
     @Test
     fun testPositiveSubtraction(){
-
+        val res = calculator.subtract(10.0, 2.0);
+        assertEquals<Double>(res, 8.0)
     }
 
     @Test
     fun testNegativeSubtraction(){
-
+        val res = calculator.subtract(-10.0, -2.0);
+        assertEquals<Double>(res, -8.0)
     }
 
     @Test
     fun testPositiveMultiply(){
-
+        val res = calculator.multiply(10.0, 2.0);
+        assertEquals<Double>(res, 20.0)
     }
 
     @Test
     fun testNegativeMultiply(){
-
+        val res = calculator.multiply(-10.0, -2.0);
+        assertEquals<Double>(res, 20.0)
     }
 
     @Test
     fun testPositiveDivision(){
-
+        val res = calculator.divide(10.0, 2.0);
+        assertEquals<Double>(res, 5.0)
     }
 
     @Test
     fun testNegativeDivision(){
-
+        val res = calculator.divide(-10.0, -2.0);
+        assertEquals<Double>(res, 5.0)
     }
 
     @Test
-    fun testErrorCases(){
-
+    fun testDivideByZeroError(){
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(10.0, 0.0)
+        }
     }
+
 }
