906c03b50d5b2b4bc4e85066ac63c933811905ce
O Csm de JP <yp.devkl@gmail.com>
2025-02-23 20:05:43 -0600

    Kotlin version and dependencies problem solved, and first version of Calculator class made.

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,19 @@
+package org.example
+
+class Calculator{
+    fun add(a : Double, b : Double) : Double{
+        return a + b;
+    }
+
+    fun subtract(a : Double, b : Double) : Double{
+        return a - b;
+    }
+
+    fun multiply(a : Double, b : Double) : Double{
+        return a * b;
+    }
+
+    fun divide(a : Double, b : Double) : Double{
+        return a / b;
+    }
+}
